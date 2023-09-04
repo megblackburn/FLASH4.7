7a4a11d338700eecabf46b05420ab230f62b2c87
Meg Blackburn <123603777+megblackburn@users.noreply.github.com>
2023-09-04 11:26:20 +0100

    Update cool_integrate.F90

--- KHshiftcool_current/cool_integrate.F90
+++ KHshiftcool_current/cool_integrate.F90
@@ -1,3 +1,10 @@
+!!****if* source/Simulation/SimulationMain/KHshiftcool_current
+!! uses a fourth-order Runge-Kutta method to integrate the Sutherland-Dopita cooling function
+!! and compute the new internal energy. This was handled by a third-order method provided 
+!! by SVODE in Flash 2
+!! from RF
+
+
 subroutine cool_integrate (e,t,dt)
   implicit none
   real, intent(INOUT) :: e
